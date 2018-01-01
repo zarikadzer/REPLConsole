5cc7354e7f333cd9835ae9e6faf383fbad7ddf65
Anton Nazarenko <9337044+zarikadzer@users.noreply.github.com>
2018-01-01 13:10:56 +0200

    ScriptState support added. Every new submission will continue the previous scriptState.
    Bug with state on warnings  has been fixed.

--- REPL.Engine/ReplEngineCS.cs
+++ REPL.Engine/ReplEngineCS.cs
@@ -4,13 +4,12 @@
     using Microsoft.CodeAnalysis.CSharp.Scripting;
     using Microsoft.CodeAnalysis.Scripting;
     using System.IO;
-	using System.Reflection;
-	using System.Linq;
+    using System.Reflection;
+    using System.Linq;
 
-	public class ReplEngineCS : ReplEngineBase
+    public class ReplEngineCS : ReplEngineBase
     {
-        public ReplEngineCS(Guid sessionId) : base(sessionId)
-        {
+        public ReplEngineCS(Guid sessionId) : base(sessionId) {
         }
 
         public static implicit operator WeakReference<ReplEngineBase>(ReplEngineCS instance) {
@@ -19,17 +18,17 @@
 
         public override void InitEngineWithAssembly(Assembly parentAssembly) {
             string appDir = Path.GetDirectoryName(parentAssembly.GetFiles().FirstOrDefault().Name);
-            
-			//All dependend assemblies
-			foreach (var dll in Directory.EnumerateFiles(appDir, "*.dll")) {
+
+            //All dependend assemblies
+            foreach (var dll in Directory.EnumerateFiles(appDir, "*.dll")) {
                 var loadDllCmd = $"#r \"{dll}\"";
                 var initRefsResult = Eval(loadDllCmd);
                 HandleOutputEvent(loadDllCmd + initRefsResult);
             }
-            
-			//Add reference to the executable assembly
-			var loadExeCmd = $"#r \"{parentAssembly.GetFiles().FirstOrDefault().Name}\"";
-			var initExeResult = Eval(loadExeCmd);
+
+            //Add reference to the executable assembly
+            var loadExeCmd = $"#r \"{parentAssembly.GetFiles().FirstOrDefault().Name}\"";
+            var initExeResult = Eval(loadExeCmd);
             HandleOutputEvent(loadExeCmd + initExeResult);
 
             //Initial references and usings
@@ -46,24 +45,23 @@
             }
         }
 
-        public override Script GetScriptSession(string command, ScriptOptions options = null)
-        {
-            if (!ScriptSessions.TryGetValue(SessionId, out var existedScript))
-            {
-                var newScript = CSharpScript.Create(command, options);
-                return newScript;
+        public override Tuple<Script, ScriptState> GetScriptSession(string command, ScriptOptions options = null) {
+            if (!ScriptSessions.TryGetValue(SessionId, out var existedSession)) {
+                var newSession = new Tuple<Script, ScriptState>(CSharpScript.Create(command, options), null);
+                return newSession;
             }
+            var existedScript = existedSession.Item1;
             var next = existedScript.ContinueWith(command, options);
-            return next;
+            return new Tuple<Script, ScriptState>(next, existedSession.Item2);
         }
 
-		private string GetInitFileContents() {
-			var resourceName = "REPL.Engine.Properties.Resources.resources";
-			using (var stream = GetType().Assembly.GetManifestResourceStream(resourceName))
-			using (var reader = new StreamReader(stream)) {
-				return reader.ReadToEnd();
-			}
-		}
+        private string GetInitFileContents() {
+            var resourceName = "REPL.Engine.Properties.Resources.resources";
+            using (var stream = GetType().Assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream)) {
+                return reader.ReadToEnd();
+            }
+        }
 
     }
 }
